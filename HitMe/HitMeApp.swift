ce8ef6ecd84cee5a66cfd32005df8c7533335235
venkat2789 <venkatachalapathy.27@gmail.com>
2022-04-09 12:44:28 -0400

    Initial Commit

---
+++ HitMe/HitMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HitMeApp.swift
+//  HitMe
+//
+//  Created by Venkatachalapathy Sivaprakasam on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct HitMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
